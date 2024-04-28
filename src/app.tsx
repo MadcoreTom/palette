dff8e5737f65bb893110d5fce1415963ed56441b
MadcoreTom <madcoretom@gmail.com>
2024-04-28 15:39:19 +1000

    Added title bar

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,28 @@ import * as React from "react";
 import { createRoot } from 'react-dom/client';
 import { ModeForm } from "./mode-form";
 import { Colours } from "./colours";
-
+import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import RssFeedIcon from '@mui/icons-material/RssFeed';
 
 function App() {
     const canvas = React.useRef(null);
 
     return <div>
-        <canvas width="360" height="100" style={{ width: "100%"}} ref={canvas}></canvas>
-        <ModeForm />
-        <Colours />
-
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Palette
+                </Typography>
+                <Button color="inherit" startIcon={<GitHubIcon/>} href="https://github.com/MadcoreTom/palette">Source Code</Button>
+                <Button color="inherit" startIcon={<RssFeedIcon/>} href="https://madcoretom.com">MadcoreTom.com</Button>
+            </Toolbar>
+        </AppBar>
+        <canvas width="360" height="100" ref={canvas}></canvas>
+        <Container maxWidth="xl">
+            <ModeForm />
+            <Colours />
+        </Container>
     </div>
 }
 
