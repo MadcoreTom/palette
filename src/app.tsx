e53955cf6e73b7ed9c1339fa623cf46842ec7d16
MadcoreTom <madcoretom@gmail.com>
2024-04-28 15:51:52 +1000

    Fixed the font, added instructions

--- src/app.tsx
+++ src/app.tsx
@@ -15,14 +15,21 @@ function App() {
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Palette
                 </Typography>
-                <Button color="inherit" startIcon={<GitHubIcon/>} href="https://github.com/MadcoreTom/palette">Source Code</Button>
-                <Button color="inherit" startIcon={<RssFeedIcon/>} href="https://madcoretom.com">MadcoreTom.com</Button>
+                <Button color="inherit" startIcon={<GitHubIcon />} href="https://github.com/MadcoreTom/palette">Source Code</Button>
+                <Button color="inherit" startIcon={<RssFeedIcon />} href="https://madcoretom.com">MadcoreTom.com</Button>
             </Toolbar>
         </AppBar>
-        <canvas width="360" height="100" ref={canvas}></canvas>
-        <Container maxWidth="xl">
+        <Container maxWidth="lg">
+            <canvas width="360" height="100" ref={canvas}></canvas>
             <ModeForm />
             <Colours />
+            <Typography marginTop={2}>
+                This tool shows the colours in HSL (hue, saturation, lightness) with hue on the X-axis, lightness on the y-axis and saturation on a slider.<br />
+                It maps this 'rainbow' to the closest colours in your palette. It can help you develop a palette, or identify gaps.<br />
+            </Typography><Typography marginTop={2}>
+                The <b>Presets</b> include a few pre-made palettes from some well known oldschool computers, and some I like from Lospec.com<br />
+                The <b>Raw Editor</b> lets you copy/paste colour codes to/from your favourite tools
+            </Typography>
         </Container>
     </div>
 }
